0c3b908e23bfb38d4266bb3aa9eace0861836982
ItzaCarrillo <vichytacarrillo@gmail.com>
2019-11-25 09:14:48 -0600

    cambie css en casas y en el menu , con los botones de sorting hat que te mandan a cada casa tambien filtra la los datos por casa funciona para filtrar por casa y subfuncion para filtrar dentro de el primer filtro

--- src/data.js
+++ src/data.js
@@ -1,29 +1,30 @@
-  /* Manejo de data */
-// esta es una función de ejemplo
 const URL_POTTER = "https://raw.githubusercontent.com/Laboratoria/GDL004-data-lovers/master/src/data/potter/potter.json";
-let potterData;
+let potterData ;
+let filteredCharacters;
+let subFilteredCharacters;
 
-window.filterBy = async function filterBy(fromElement, elementValue) {
+async function filterBy(filterKey, filterValue) {
   await getPotterData();
-  console.log(potterData);
-  console.log(fromElement);
-  let filteredCharacters = potterData.filter(movieCharacter => (movieCharacter[fromElement] === elementValue));
+  //  console.log(potterData);
+  //console.log(filterKey + " and " + filterValue);
+  filteredCharacters = potterData.filter(movieCharacter => (movieCharacter[filterKey] === filterValue));
   console.log(filteredCharacters)
-  return filteredCharacters;
+  return "filteredDone";
 };
 
-/*window.subFilterBy = function filterBy(filteredCharacters, fromElement, elementValue) {
-  console.log(filteredCharacters);
-  console.log(fromElement);
-  var subFilteredCharacters = filteredCharacters.filter(movieCharacter => (movieCharacter[fromElement] === elementValue));
-  console.log(subFilteredCharacters)
-  return subFilteredCharacters;
-};*/
+async function subFilterBy(filter1Key, filter1Value, filter2Key, filter2Value) {
+    let filterByResult = await filterBy(filter1Key, filter1Value);
+    //console.log("el resultado de FilterBy es: " + filterByResult);
+    //console.log(filteredCharacters);
+    subFilteredCharacters = filteredCharacters.filter(movieCharacter => (movieCharacter[filter2Key] === filter2Value));
+    console.log(subFilteredCharacters);
+    return subFilteredCharacters;
+};
 
-window.getPotterData = async function getPotterData() {//leer nuestro JSON
+async function getPotterData() {//leer nuestro JSON
       let response = await fetch(URL_POTTER); //espera await una respuesta de el fetch
       potterData = await response.json();
       return potterData;
   };
 
-  export default "DataPotter";
+  export{filteredCharacters, subFilteredCharacters, filterBy, subFilterBy}
--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
-/* import POTTER from './data/potter/potter.js'
- //import{houses} from './data.js
-/*import POTTER from './data/potter/potter.json'*/
-//console.log(POTTER)
-import datajs from './data.js';
 
-window.NoneBlock = function NoneBlock(fromId) {
+import {filteredCharacters, subFilteredCharacters, filterBy, subFilterBy} from '/data.js';
+
+function NoneBlock(fromId) {
   let idArray = ["Start", "Characters", "sortingHat", "Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"];
   console.log(fromId)
   for (let id in idArray) {
@@ -15,8 +12,15 @@ window.NoneBlock = function NoneBlock(fromId) {
     else {
       document.getElementById(idArray[id]).style.display = "none";
     }
-   }
+  }
 }
+let startItem = document.getElementsByClassName("startItem");
+startItem[0].addEventListener("click", function() {NoneBlock("Start")});
+let charactersItem = document.getElementsByClassName("charactersItem");
+charactersItem[0].addEventListener("click", function() {NoneBlock("Characters")});
+let sortingHatItem = document.getElementsByClassName("sortingHatItem");
+sortingHatItem[0].addEventListener("click", function() {NoneBlock("sortingHat")});
+
 let sortingHatBtn = document.getElementById("sortingHatBtn");
 sortingHatBtn.addEventListener("click", function() {NoneBlock("sortingHat")});
 let gryffindorBtn = document.getElementById("gryffindorBtn");
@@ -28,16 +32,24 @@ hufflepuffBtn.addEventListener("click", function() {NoneBlock("Hufflepuff")});
 let ravenclawBtn = document.getElementById("ravenclawBtn");
 ravenclawBtn.addEventListener("click", function() {NoneBlock("Ravenclaw")});
 
-document.addEventListener("click", () => {
-  filterBy("house", "Gryffindor")
-  .then(patronus => {
+gryffindorBtn.addEventListener("click",() => {filterBy("house","Gryffindor");
+                                console.log(filteredCharacters);});
+slytherinBtn.addEventListener("click",() =>{filterBy("house","Slytherin");});
+hufflepuffBtn.addEventListener("click",() =>{filterBy("house","Hufflepuff");});
+ravenclawBtn.addEventListener("click",() =>{filterBy("house","Ravenclaw");});
+
+document.addEventListener("click", () =>
+  {subFilterBy("patronus", "stag","species","human");
+  console.log(subFilteredCharacters);});
+
+/* .then(patronus => {
       console.log(patronus);
     })
     .catch(error => {
         console.error("Error al filtrar patronus");
         console.log(error);
-    });
-});
+    });*/
+
 
 /*document.addEventListener("click", async () => {
   let subFiltered = await filterBy("House", "Gryffindor")
